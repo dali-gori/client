29a93a7f9ffdfb56001653c54d665944edb95e83
Dean Yurukov <dean.yurukov2008@gmail.com>
2025-09-06 18:43:26 +0300

    plans page updated

--- src/components/SinglePlan.jsx
+++ src/components/SinglePlan.jsx
@@ -3,13 +3,8 @@ const SinglePlan = ({ isBigger = false, name, pinsAllowed, priceInEur, priceInLv
         <article className={`${isBigger && "bigger"}`}>
             <h3>{name}</h3>
             <p>Позволява право на {pinsAllowed} пина</p>
-            {/* <span>{priceInLv.toFixed(2)}лв. ({priceInEur.toFixed(2)}€) / на месец</span> */}
-            <span>
-                <b>{priceInLv.toFixed(2)}лв.</b>
-                <span> | </span>
-                <b>{priceInEur.toFixed(2)}€</b>
-                <span> / на месец</span>
-            </span>
+            <span>{priceInLv.toFixed(2)}лв. / {priceInEur.toFixed(2)}€</span>
+            <p>На месец</p>
             <button type="button">Избери</button>
         </article>
     );
--- src/pages/PlansPage.jsx
+++ src/pages/PlansPage.jsx
@@ -3,25 +3,40 @@ import SinglePlan from "../components/SinglePlan";
 const PlansPage = () => {
     return (
         <section id="plans">
-            <SinglePlan
-                name={"Безплатен"}
-                pinsAllowed={1}
-                priceInLv={0}
-                priceInEur={0}
-            />
-            <SinglePlan
-                isBigger={true}
-                name={"Разширен"}
-                pinsAllowed={3}
-                priceInLv={4.99}
-                priceInEur={2.50}
-            />
-            <SinglePlan
-                name={"Бизнес"}
-                pinsAllowed={10}
-                priceInLv={14.99}
-                priceInEur={7.50}
-            />
+            <h1>Планове</h1>
+
+            <article>
+                <SinglePlan
+                    name={"Безплатен"}
+                    pinsAllowed={1}
+                    priceInLv={0}
+                    priceInEur={0}
+                />
+                <SinglePlan
+                    isBigger={true}
+                    name={"Разширен"}
+                    pinsAllowed={3}
+                    priceInLv={4.99}
+                    priceInEur={2.50}
+                />
+                <SinglePlan
+                    name={"Бизнес"}
+                    pinsAllowed={10}
+                    priceInLv={14.99}
+                    priceInEur={7.50}
+                />
+            </article>
+
+            <article className="enterprise">
+                <div>
+                    <h3>Enterprise</h3>
+                    <p>Започва от</p>
+                    <span>499.99 лв./ 250.00€</span>
+                    <p>На месец</p>
+                    <p>Позволява право на над 10 пина</p>
+                </div>
+                <button type="button">Избери</button>
+            </article>
         </section>
     );
 }
